6312c7bb5ac77aa6c807ba345736880c6420612a
Aakash Kumar <your-email@gmail.com>
2025-12-17 15:52:13 +0530

    LRU Cache complete

--- LRU_Cache/LRUCache.cpp
+++ LRU_Cache/LRUCache.cpp
@@ -12,3 +12,23 @@ int LRUCache::get(int key){
     dll.addFront(node);
     return node->value;
 }
+void LRUCache::put(int key,int value){
+    if(mp.find(key) != mp.end()){
+        Node* node = mp[key];
+        node->value = value;
+        dll.removeNode(node);
+        dll.addFront(node);
+        return;
+
+    }
+    if(mp.size() == capacity){
+        Node* LRU = dll.removeLast();
+        if(LRU != nullptr){
+            mp.erase(LRU->key);
+            delete LRU;
+        }
+    }
+    Node* newNode =new Node(key,value);
+    dll.addFront(newNode);
+    mp[key] = newNode;
+}
