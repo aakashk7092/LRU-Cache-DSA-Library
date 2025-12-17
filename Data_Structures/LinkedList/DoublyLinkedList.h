2a5de8e3b2ff86143aa047b5a8d54eab3fbd0e9d
Aakash Kumar <your-email@gmail.com>
2025-12-17 11:07:57 +0530

    Separate class declaration and implementation for doubly linked list

--- Data_Structures/LinkedList/DoublyLinkedList.h
+++ Data_Structures/LinkedList/DoublyLinkedList.h
@@ -1,1 +1,24 @@
+class Node{
+    public:
+        int key;
+        int value;
+        Node* next;
+        Node* prev;
 
+        Node(int key,int value){
+            this->key = key;
+            this->value = value;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+
+};
+class DoublyLinkedList{
+    public:
+    Node* head ;
+    Node* tail;
+    DoublyLinkedList();
+        void  addFront(Node* node);
+        void  removeNode(Node* node);
+        Node* removeLast();
+};
