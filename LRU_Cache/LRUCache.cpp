8a429a82388506f1d617c12e4a74b669607c2833
Aakash Kumar <your-email@gmail.com>
2025-12-17 15:40:27 +0530

    Implement get operation for LRUCache

--- LRU_Cache/LRUCache.cpp
+++ LRU_Cache/LRUCache.cpp
@@ -3,3 +3,12 @@
 LRUCache::LRUCache(int capacity){
     this->capacity = capacity;
 }
+int LRUCache::get(int key){
+    if(mp.find(key) == mp.end()){
+        return -1;
+    }
+    Node* node = mp[key];
+    dll.removeNode(node);
+    dll.addFront(node);
+    return node->val;
+}
