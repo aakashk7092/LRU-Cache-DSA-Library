b2141e7cbf71b5527d07d41c4dad3b6a2ff5eeed
Aakash Kumar <your-email@gmail.com>
2025-12-17 11:28:10 +0530

    Add Front function Implemented

--- Data_Structures/LinkedList/DoublyLinkedList.cpp
+++ Data_Structures/LinkedList/DoublyLinkedList.cpp
@@ -1,1 +1,15 @@
-
+#include "DoublyLinkedList.h"
+DoublyLinkedList::DoublyLinkedList(){
+    head = nullptr;
+    tail = nullptr;
+};
+void DoublyLinkedList::addFront(Node* node){
+    if(head == nullptr){
+        head = tail = node;
+        return;
+    }else{
+        node->next = head;
+        head->prev = node;
+        head = node;
+    }
+}
