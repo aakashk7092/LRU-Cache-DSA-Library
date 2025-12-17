03d913adb05881afac1b9d63e360128c832b5b50
Aakash Kumar <your-email@gmail.com>
2025-12-17 14:27:17 +0530

    Linked Section is Done

--- Data_Structures/LinkedList/DoublyLinkedList.cpp
+++ Data_Structures/LinkedList/DoublyLinkedList.cpp
@@ -13,3 +13,41 @@ void DoublyLinkedList::addFront(Node* node){
         head = node;
     }
 }
+void DoublyLinkedList::removeNode(Node* node){
+    if(node == nullptr) return;
+    if(node == head && node == tail){
+        head = nullptr;
+        tail= nullptr;
+        return ;
+    }
+    if(node == head){
+        head = head->next;
+        head->prev = nullptr;
+        return;
+    }
+    if(node == tail)
+{
+        tail = tail->prev;
+        tail->next = nullptr;
+        return;
+    }
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+}
+Node* DoublyLinkedList::removeLast(){
+    if(tail ==nullptr){
+        return nullptr;
+    }
+    Node* removedNode = tail;
+
+    if(head == tail){
+        head = nullptr;
+        tail = nullptr;
+        return removedNode;
+    }
+    tail = tail->prev;
+    tail->next = nullptr;
+    removedNode->next = nullptr;
+    removedNode->prev = nullptr;
+    return removedNode;
+}    
